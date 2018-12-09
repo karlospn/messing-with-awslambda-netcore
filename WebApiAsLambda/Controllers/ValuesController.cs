413c83c2a4f7cdb5607e4289c87edc212789b054
Carlos Pons <karlospn@gmail.com>
2018-12-09 19:28:29 +0100

    Add net core 2.1 webapi as lambda

---
+++ WebApiAsLambda/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DummyWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("method1")]
+        public ActionResult<string> GetMethod1()
+        {
+            return "method_1";
+        }
+
+        [HttpGet("method2")]
+        public ActionResult<string> GetMethod2()
+        {
+            return "method_2";
+        }
+
+      
+    }
+}
